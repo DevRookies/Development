acb6fdf69174dec804d9da6e38f0764c050fe709
youis11 <lluis.moreu@gmail.com>
2018-10-08 20:45:32 +0200

    camera mov inverted

--- DevRookies/DevRookies/Scene.cpp
+++ DevRookies/DevRookies/Scene.cpp
@@ -59,16 +59,16 @@ bool Scene::Update(float dt)
 		App->SaveGame();
 
 	if(App->input->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT)
-		App->render->camera.y -= 1;
-
-	if(App->input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT)
 		App->render->camera.y += 1;
 
+	if(App->input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT)
+		App->render->camera.y -= 1;
+
 	if(App->input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT)
-		App->render->camera.x -= 1;
+		App->render->camera.x += 1;
 
 	if(App->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
-		App->render->camera.x += 1;
+		App->render->camera.x -= 1;
 
 	//App->render->Blit(img, 0, 0);
 	App->map->Draw();
